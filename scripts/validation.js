c474efe6add59384dfcec11b18d7c47053204887
DevmithU <79127952+DevmithU@users.noreply.github.com>
2023-10-23 08:03:49 +0530

    -fix form validation

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,76 +1,60 @@
-let errorFlag = false;
-
+let errorFlag = true;
 
 function validateField(fieldId, fieldName) {
     const field = document.getElementById(fieldId);
     const errorField = document.getElementById(fieldId + "Error");
+    errorField.textContent = "";
+    field.classList.remove("input-value-error");
 
     if (!field.value.trim()) {
         errorField.textContent = `${fieldName} is required.`;
-        disableButton();
-
+        errorFlag = false;
     } else {
-        errorField.textContent = "";
-        enableButton();
-
-        // Check for email validation
         if (fieldId === "email") {
             const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
             if (!emailRegex.test(field.value)) {
                 errorField.textContent = "Invalid email address";
                 field.classList.add("input-value-error");
-                disableButton();
-            } else {
-                field.classList.remove("input-value-error");
-                errorField.textContent = "";
-                enableButton();
+                errorFlag = false;
             }
         }
 
-        // Check for firstName and lastName validation
         if (fieldId === "firstName" || fieldId === "lastName") {
-            const nameRegex = /^[a-zA-Z]+$/; // This ensures the name contains only letters
+            const nameRegex = /^[a-zA-Z]+$/;
             if (!nameRegex.test(field.value)) {
                 errorField.textContent = `${fieldName} should not contain numbers or symbols.`;
                 field.classList.add("input-value-error");
-                disableButton();
-            } else {
-                field.classList.remove("input-value-error");
-                errorField.textContent = "";
-                enableButton();
+                errorFlag = false;
             }
         }
     }
 }
 
-function resetField(fieldId){
+function resetField(fieldId) {
     const field = document.getElementById(fieldId);
-    field.value='';
-
+    field.value = '';
 }
-function disableButton(){
-    errorFlag = false;
+
+function disableButton() {
     document.getElementById('submitButton').classList.add('btn-disabled');
-
 }
-function enableButton(){
-    errorFlag = true;
+
+function enableButton() {
     document.getElementById('submitButton').classList.remove('btn-disabled');
-
-
 }
 
 function submitMessage() {
+    errorFlag = true;  // Reset errorFlag before validating
+
     validateField('email', 'Email');
     validateField('firstName', 'First Name');
     validateField('lastName', 'Last Name');
     validateField('address', 'Address');
 
-
-    if (errorFlag === true) {
-        //Form submission code
-
-
+    if (!errorFlag) {
+        disableButton();
+    } else {
+        // Form submission code
         resetField('firstName');
         resetField('lastName');
         resetField('email');
@@ -78,8 +62,8 @@ function submitMessage() {
         showMessage();
         console.log('Form data Submitted');
     }
-
 }
+
 function showMessage() {
     document.getElementById('overlay').style.display = 'block';
 }
